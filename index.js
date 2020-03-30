f5f0e5bab276c897aabbd1b7c840fd0439c2ebd3
DmytroKulish1 <62491404+DmytroKulish1@users.noreply.github.com>
2020-03-29 20:09:58 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cards = [...document.querySelectorAll(".col")];
+const cards = [...document.querySelectorAll(".col-sm-12")];
 const topBlock = document.getElementById("card");
 cards.forEach(item => item.addEventListener("click", function() {
   topBlock.innerHTML = item.innerHTML;
